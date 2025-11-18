668923c67822d94c2f37e62836806479a50d9de1
sb0205 <bhowmik.sb0205@gmail.com>
2025-11-18 15:32:38 +0600

    first commit

---
+++ src/test/java/com/ctos/dummy/library/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ctos.dummy.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+    @Test
+    void contextLoads() {
+
+    }
+}
